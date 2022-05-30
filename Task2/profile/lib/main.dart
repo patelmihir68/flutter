a910aeae94a51f60fcff4169ff4cea3b735e86aa
mihir.patel <mihir.patel@coruscatesolutions.in>
2022-05-30 09:59:18 +0530

    task2

---
+++ Task2/profile/lib/main.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const ProfilePage(title: 'Profile Page'),
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        //title: Text(widget.title)
+        backgroundColor: Colors.white,
+        leading:IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0,
+        actions:  <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: IconButton(
+                icon: const Icon(Icons.create, color: Colors.black),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+          ),
+        ],
+      ),
+      body:Column(children: <Widget>[
+      Row(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+
+        children: <Widget>[
+
+            Container(
+              decoration:BoxDecoration(
+                  borderRadius:BorderRadius.circular(10),
+                  color:Colors.green
+              ),
+              child: Container(
+                height: 120.0,
+                width: 120.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/xyz.jpg'),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+
+              ),
+            ),
+            Container(
+                decoration:const BoxDecoration( ),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    style: TextStyle(
+                      fontSize: 36.0,
+                      color: Colors.black,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: '104\n', style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: 'Design posted', style: TextStyle(fontSize: 16.0)),
+                    ],
+                  ),
+                )
+            ),
+            Container(
+              decoration:const BoxDecoration(
+              ),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontSize: 36.0,
+                    color: Colors.black,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(text: '12\n', style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(text: 'Board created', style: TextStyle(fontSize: 16.0)),
+                  ],
+                ),
+              )
+            )
+          ],
+      ),
+        Row( children: const <Widget>[
+          Icon(Icons.phone),
+          Text("+91 9408448479")
+        ]
+        ),
+        Row( children: const <Widget>[
+          Icon(Icons.email),
+          Text("mihir.patel@dhiwise.com")
+        ]
+        ),
+        Row(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.black, width: 2),
+                    right: BorderSide(color: Colors.black, width: 2),
+                    bottom: BorderSide(color: Colors.black, width: 2)
+                  ),
+              ),
+              child:   RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontSize: 36.0,
+                    color: Colors.black,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(text: "\$2084\n", style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(text: 'Balance', style: TextStyle(fontSize: 16.0)),
+                  ],
+                ),
+              ),
+            ),
+
+            Container(
+
+
+              decoration: const BoxDecoration(
+                border: Border(
+                    top: BorderSide(color: Colors.black, width: 2),
+                    left: BorderSide(color: Colors.black, width: 2),
+                    bottom: BorderSide(color: Colors.black, width: 2)
+                ),
+              ),
+              child: RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                style: TextStyle(
+                  fontSize: 36.0,
+                  color: Colors.black,
+                ),
+                children: <TextSpan>[
+                  TextSpan(text: "14\n", style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: 'Order', style: TextStyle(fontSize: 16.0)),
+                ],
+              ),
+            ),
+            )
+          ],
+        ),
+        Row(
+
+        )
+
+      ]
+      )
+    );
+  }
+}
